59953ddb9cec7976c501108983edf0447d00e425
El-brayan502 <brayanphonkponk@gmail.com>
2024-08-02 16:55:59 -0600

    Actualizar main-grupos.js

--- plugins/main-grupos.js
+++ plugins/main-grupos.js
@@ -1,12 +1,12 @@
 import fetch from 'node-fetch'
 
 let handler  = async (m, { conn, usedPrefix, command }) => {
-let img = await (await fetch(`https://i.ibb.co/3N4StyG/file.jpg`)).buffer()
+let img = await (await fetch(`https://telegra.ph/file/3d2ef259c1c976692be2d.jpg`)).buffer()
 const more = String.fromCharCode(8206)
 const readMore = more.repeat(4001)
 let txt = `*Hola!, te invito a unirte a los grupos oficiales de del Bot para convivir con la comunidad :D*
 
-1- THE BROLY - BOT
+1- THE BROLY-BOT
 *✰* ${group}
 
 *─ׄ─ׅ─ׄ⭒─ׄ─ׅ─ׄ⭒─ׄ─ׅ─ׄ⭒─ׄ─ׅ─ׄ⭒─ׄ─ׅ─ׄ⭒─ׄ─ׅ─ׄ*
