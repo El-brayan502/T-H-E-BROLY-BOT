e419da4cc385b60cba83f2c18e6e53d12b7f523b
El-brayan502 <brayanphonkponk@gmail.com>
2024-08-02 11:16:38 -0600

    Actualizar _fakeReply.js

--- plugins/_fakeReply.js
+++ plugins/_fakeReply.js
@@ -7,7 +7,7 @@ let img = await (await fetch(`https://tinyurl.com/2c5hk765`)).buffer()
     contextInfo: {
     	isForwarded: true,
     forwardedNewsletterMessageInfo: {
-      newsletterJid: "120363191779210764@newsletter",
+      newsletterJid: "120363312092804854@newsletter",
       serverMessageId: 100,
       newsletterName: namechannel,
     },
