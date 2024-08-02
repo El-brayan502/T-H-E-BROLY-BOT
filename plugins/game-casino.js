e22a12c69b44f8736d429c7a94fc16b0daff6fe1
El-brayan502 <brayanphonkponk@gmail.com>
2024-08-02 16:38:39 -0600

    Actualizar game-casino.js

--- plugins/game-casino.js
+++ plugins/game-casino.js
@@ -2,7 +2,7 @@
 	    
         count = count ? /all/i.test(count) ? Math.floor(global.db.data.users[m.sender].limit / buatall) : parseInt(count) : args[0] ? parseInt(args[0]) : 1
         count = Math.max(1, count)
-        if (args.length < 1) return conn.reply(m.chat, '🛂 Ingresa la cantidad de ' + `*🪙 BrolyCoins*` + ' que deseas aportar contra' + ` *Ai Hoshino*` + `\n\n` + '`Ejemplo:`\n' + `> *${usedPrefix + command}* 100`, m, rcanal)
+        if (args.length < 1) return conn.reply(m.chat, '🛂 Ingresa la cantidad de ' + `*🪙 BrolyCoins*` + ' que deseas aportar contra' + ` *The Broly-bot*` + `\n\n` + '`Ejemplo:`\n' + `> *${usedPrefix + command}* 100`, m, rcanal)
 
         if (user.limit >= count * 1) {
             user.limit -= count * 1
